9bc7afda41b201f62b1fe311b90a237839495554
Next <digijoera47@hotmail.com>
2016-08-02 13:02:07 +0800

    Adding views and seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Brand.create(:name => 'Alpha')
+Brand.create(:name => 'Beta')
+Brand.create(:name => 'Gama')
